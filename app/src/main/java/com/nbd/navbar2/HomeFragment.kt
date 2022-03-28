d995b3232f167a09aaef7aa8c17bb799d455cc24
Ignacio Ordaz <narseg.f@gmail.com>
2022-03-28 12:01:52 -0600

    2a clase, argumento pasado, solucion del error de proyecto anterior que no me dejaba usar safeargs

--- app/src/main/java/com/nbd/navbar2/HomeFragment.kt
+++ app/src/main/java/com/nbd/navbar2/HomeFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.nbd.navbar2.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding= FragmentHomeBinding.inflate(layoutInflater)
+
+        //onclick
+        //action_homeFragment_to_insideHomeFragment2
+        val navController= findNavController()
+        binding.buttonHomeToInside.setOnClickListener{
+            //Toast.makeText(context, "prueba", Toast.LENGTH_SHORT).show()
+            //navController.navigate(R.id.action_homeFragment_to_insideHomeFragment2)
+        val directions=HomeFragmentDirections.actionHomeFragmentToInsideHomeFragment2("desde el home")
+        navController.navigate(directions)
+        }
+
+        return binding.root
+
     }
 
 
--- app/src/main/java/com/nbd/navbar2/InsideHomeFragment.kt
+++ app/src/main/java/com/nbd/navbar2/InsideHomeFragment.kt
@@ -5,15 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nbd.navbar2.databinding.FragmentInsideHomeBinding
 
 class InsideHomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentInsideHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inside_home, container, false)
+        binding= FragmentInsideHomeBinding.inflate(layoutInflater)
+        val argumentoRecibido=arguments?.getString("argumentoPasado")
+        return binding.root
     }
 
 }
